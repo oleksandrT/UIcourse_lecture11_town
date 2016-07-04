f24c2adf05750e6101c2654c2f2567b75997fb34
Aleksandr Tkachenko <Alexandr.Tkachenko@3shape.com>
2016-07-04 13:58:55 +0300

    Rewrite constructors

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-var houseEL = document.querySelector('.house-withelevator');
+//var houseEL = document.querySelector('.house-withelevator');
+var houseEL = document.querySelector('.js-house');
 
-var house = new House(houseEL);
+var house = new House(houseEL, 6);
--- module.js
+++ module.js
@@ -1,94 +1,96 @@
 ;(function () {
 
-    this.House = function (el) {
-        console.log('house');
-        console.log(el);
+    // Define House constructor
+    this.House = function (element, floors) {        
+        // Public properties
+        this.element = element;
+        this.floors = floors;
+
+        _initHouse.call(this);
+    };
+
+    // Define Person constructor
+    function Person(floor, room) {
+        this.floor = floor;
+        this.room = room;
+
+        _renderPerson.call(this);
+    }
+
+    // Public methods
+    Person.prototype.callElevator = function () {
+        console.log('Call elevator');
+    };
+
+    // Private methods
+    function _initHouse() {
+        _renderHouse.call(this);
+
+        // Find room on 2nd floor on right side
+        var initRoom = this.element.querySelector(".house_right").querySelectorAll(".house_flat")[this.floors - 2];
+        var person = new Person(2, initRoom);
+
+        _addEventListeners.call(this);
+    }
+
+    function _renderHouse() {
+        var docFrag = document.createDocumentFragment();
+
+        // Create left side of house
+        var leftSide = document.createElement("div");
+        leftSide.className = "house_left";
+
+        // Create right side of house
+        var rightSide = document.createElement("div");
+        rightSide.className = "house_right";
+
+        // Fill with flats
+        for(var i=1, n = this.floors; i <= n; i++) {
+            var flatL = document.createElement("div"),
+                flatR = document.createElement("div");
+            flatL.className = "house_flat";
+            flatR.className = "house_flat";
+            flatL.setAttribute('data-floor', i);
+            flatR.setAttribute('data-floor', i);
+            leftSide.appendChild(flatL);
+            rightSide.appendChild(flatR);
+        }
+
+        // Create mine with elevatore
+        var mine = document.createElement("div");
+        mine.className = "house_mine";
+        var elevator = document.createElement("div");
+        elevator.className = "house_elevator";
+        var light = document.createElement("div");
+        light.className = "house_light";
+        elevator.appendChild(light);
+        mine.appendChild(elevator);
+
+        docFrag.appendChild(leftSide);
+        docFrag.appendChild(mine);
+        docFrag.appendChild(rightSide);
+        this.element.appendChild(docFrag);
+    }
+
+    function _addEventListeners() {
+        this.element.addEventListener("click", function (e) {
+            var target = e.target;
+            if (target.className == "house_flat") {
+                var targetFloor = parseInt(target.getAttribute("data-floor"), 10);
+                var person = new Person(targetFloor, target);
+            }
+        });
+    }
+
+    function _renderPerson() {
+        // Render person
+        var person = document.createElement("div");
+        person.className = "person";
+        var image = document.createElement("img");
+        image.setAttribute('src', 'images/man.svg');
+
+        person.appendChild(image);
+        this.room.appendChild(person);
     }
 
 })();
-
-
-
-
-// window.onload =
-//     function () {
-//
-//     // Init objects
-//     var mainHouse = new House( document.querySelector(".house-withelevator") );
-//     var elevator = new Elevator( document.querySelector(".house_elevator") );
-//
-// };
-
-// Define object constructors
-// function Person(floor) {
-//     this.atFloor = floor;
-//     this.callElevator = function () {
-//
-//     };
-// }
-//
-// function House(el) {
-//     this.flats = el.querySelectorAll(".house_flat");
-//
-//     var floor;
-//
-//     el.addEventListener("click", function (e) {
-//         var target = e.target;
-//         floor = target.getAttribute("data-floor");
-//         addPerson(target);
-//     });
-//
-//     function addPerson(target) {
-//         addPersonElement(target);
-//         var man = new Person();
-//     }
-//
-//     function addPersonElement(target) {
-//         if (target.className == "house_flat") {
-//             var imgEl = document.createElement("img");
-//             imgEl.setAttribute("src", "images/man.svg");
-//             var divEl = document.createElement("div");
-//             divEl.className = "person";
-//             divEl.appendChild(imgEl);
-//             target.appendChild(divEl);
-//         }
-//     }
-// }
-//
-// function Elevator(el) {
-//     var atFloor, light;
-//     var elem = el,
-//         lightElem = elem.querySelector(".house_light");
-//
-//     this.getFloor = function() {
-//         return atFloor;
-//     };
-//
-//     this.setFloor = function (value) {
-//         atFloor = value;
-//     };
-//
-//     this.turnOnLight = function () {
-//         light = "on";
-//         lightElem.className = "house_light on";
-//     };
-//
-//     this.turnOffLight = function () {
-//         light = "off";
-//         lightElem.className = "house_light on";
-//     };
-//
-//     this.uploadPerson = function () {
-//
-//     };
-//
-//     this.offloadPerson = function () {
-//
-//     };
-//
-//     function init() {
-//         atFloor = 1;
-//         light = "off";
-//     }
-//     init();
-// }
